b5df10c191e7e88a6b29ad929883d15fe1debf58
Kelly Davis <kellydavis1974@gmail.com>
2017-07-03 15:58:23 -0400

    heroku cors issues continue; trying new approach from heroku docs

--- server.js
+++ server.js
@@ -4,28 +4,7 @@
 const express=require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-const app = express()  
-
-// this is an API, so it will get requests from off-domain sources:
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-app.use(allowCrossDomain);
-
-// dev tool: environment processor
-require('dotenv').config();
-
-app.use(logger("dev"));
+const app = express() 
 
 // BodyParser makes it easy for our server to interpret data sent to it.
 // The code below is pretty standard.
@@ -33,6 +12,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" })); 
+
+// set up mongoose connection
+
+var mongoose = require("mongoose");
+
+// Use promise library
+//mongoose.Promise = require('bluebird');
+
+// Using `mongoose.connect`...
+
+var promise = mongoose.connect(
+  // connection URI
+  process.env.MONGODB_URI, 
+  // connection options
+  {
+    useMongoClient: true
+  }
+  );
+  
+// Show any mongoose errors
+promise.then(function(database){
+  db = database;
+  db.on("error", function(error) {
+  console.log("Mongoose error: ", error);
+  });
+
+  // Once connected to the db through mongoose, log a success message
+  db.once("open", function() {
+    console.log("Mongoose connection successful.");
+  });
+})
+
+
 
 // ===================================================
 // ROUTES
@@ -47,7 +59,7 @@ require("./api/mongooseApi")(app);
 
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // ==================================================
 // start our server
