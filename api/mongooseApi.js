7f9bab9f4045dd8c2330bf07bc6b8339076fca56
Kelly Davis <kellydavis1974@gmail.com>
2017-07-04 12:04:11 -0400

    updated routes for persistent data storage. still need to add user filter to favorites

--- api/mongooseApi.js
+++ api/mongooseApi.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
   app.post("/api/favorites", function(req, res) {
     // req.body should include title , artist, song_id, image url, lyrics page url 
   var favorite = new Favorite(req.body);
-
+  console.log("New favorite:",favorite);
   // Save new "Favorite" object to mongoDB
   favorite.save(function(error, doc) {
     // Send any errors to the browser
@@ -31,6 +31,7 @@ module.exports = function(app) {
 
 // Route to retrieve and show favorited articles
 app.get("/api/favorites", function(req,res){
+  console.log("retrieving favorites");
   // find favorites of currently logged-in user
   Favorite.find({ }, function(err, found){
     if(err){
@@ -50,7 +51,50 @@ app.get("/api/remove/:id", function(req,res){
     };
     res.json({song:req.params.id});
    });
-})
+});
 
+// Save new user to mongoDB
+    app.post("/api/signup", function(req, res){
+        console.log("sign up route hit");
+        var newUser = new User(req.body);
+        console.log("new user:",newUser);
+        newUser.save(function(error,user){
+            if (error) {
+                console.log("Save error",error);
+                res.json({
+                    "success":false,
+                    "message": `There was a problem: ${error}` 
+                })
+            }
+            else { 
+                console.log("Success",user.username);
+                res.json({
+                    "success":true,
+                    "user":req.body.username,
+                    "message":`Welcome, ${req.body.username}`
+                })
+            }
+        })
+    }); // end signup post route
 
+// log-in user
+    app.post("/api/login",function(req,res){
+      console.log("login route, getting:",req.body);
+      User.findOne({'username' : req.body.username}, function (err, user) {
+      if (err) console.log(err)
+      else {
+          if (user.authenticate(req.body.password)) {
+              res.json({
+                "success":true,
+                "user":req.body.username,
+                "message":`Welcome, ${req.body.username}`
+              })
+            } 
+          else
+              res.json({
+                "success":false,
+                "message":"Unable to log you in. Try again."})
+          }
+      });
+    });
 };
